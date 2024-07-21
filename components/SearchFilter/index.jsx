0dff3824217be90548787f49615a507f47f46ab5
unknown <a0548428751@gmail.com>
2024-07-21 17:41:55 +0300

    search result

--- components/SearchFilter/index.jsx
+++ components/SearchFilter/index.jsx
@@ -1,44 +1,81 @@
 "use client"
 import styles from './styles.module.scss'
 import { FaSearch } from "react-icons/fa";
-import { useState } from 'react';
-export default function SearchFilter() {
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import SearchResults from '../SearchResults';
+
+export default function SearchFilter({ type }) {
+
+  const router = useRouter()
   const [typeInput, setTypeInput] = useState(false)
+  const [isSearch, setIsSearch] = useState('')
+  const [isSearchSubject, setIsSearchSubject] = useState(true)
 
-  return (
-    <div className={styles.container}>
-      {!typeInput ?
+  // useEffect(() =>{
+  //   if(typeInput==='נושאים'){
+  //     setIsSearchSubject(true)
+  //   }
+  //   else{
+  //     setIsSearchSubject(false)
 
-        <div className={styles.buttons}>
+  //   }
+  // },[typeInput]);
 
-          <button className={styles.subjectSearch} onClick={()=>setTypeInput("נושאים")}>
-            סינון לפי נושא
-          </button>
-          <div className={styles.line}>
+  let classNameIcon = !isSearch ? 'searchIcon' : 'searchButton';
+  const handleSearch = e => {
+    console.log(e.target.value)
+    setIsSearch(e.target.value)
+    if (!isSearch) {
+      // router.push(`/result?search=${e.target.value}`)
+    }
+  }
+  return (<>
+    <div className={styles.searchAndResultContainer}>
 
+      <div className={`${styles.container} ${type === "dark" ? styles.containerDark : ""}`}>
+        {!typeInput ?
+
+          <div className={styles.buttons}>
+
+            <button className={styles.subjectSearch} onClick={() => setTypeInput("נושאים")}>
+              סינון לפי נושא
+            </button>
+            <div className={`${styles.line} ${type == "dark" ? styles.dark : ""}`}>
+
+            </div>
+            <button className={styles.freeSearch} onClick={() => setTypeInput("חיפוש ")}>
+              טקסט חופשי
+            </button>
+          </div> :
+          <div className={styles.search}>
+            <button className={styles.subjectSearch} >
+              {typeInput}
+            </button>
+            <div className={styles.line} >
+            </div>
+
+
+            <input
+              type="text"
+              name="search"
+              autoFocus
+              autoComplete="off"
+              onChange={handleSearch}
+              onBlur={() => { setTypeInput('') }}
+              defaultValue={isSearch}
+            ></input>
           </div>
-          <button className={styles.freeSearch} onClick={()=>setTypeInput("חיפוש ")}>
-            טקסט חופשי
-          </button>
-        </div> :
-        <div className={styles.search}>
-
-          <button className={styles.subjectSearch} >
-            { typeInput}
-            
-          </button>
-          <div className={styles.line} >
-          </div>
-
-
-          <input ></input>
+        }
+        <div className={`${styles[classNameIcon]}`} >
+          <FaSearch />
         </div>
-      }
-      <div className={styles.icon} onClick={()=>{setTypeInput(false)}}>
-        <FaSearch />
-
       </div>
+      {isSearchSubject &&
+        <SearchResults />
+      }
 
     </div>
+  </>
   )
 }
---
+++ components/SearchResults/index.jsx
@@ -0,0 +1,18 @@
+"use client"
+import styles from './styles.module.scss'
+import { FaSearch } from "react-icons/fa";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+export default function SearchResults() {
+
+  return (
+    <>
+      <div className={styles.container}>
+
+        <h5>שבת</h5>
+        <h5>שבת</h5>
+
+      </div>
+    </>
+  )
+}
