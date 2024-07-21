445a89c00c6a66b75e09114a5febed99c6b99ef1
Ahron Cohen <aharoncohen7@gmail.com>
2024-07-21 15:54:22 +0300

    "It works - commit before deleting temporary files"

---
+++ hooks/useAxiosReq.jsx
@@ -0,0 +1,33 @@
+"use client"
+import { axiosReq } from '@/helpers/index.js'
+import { useEffect, useState } from 'react'
+
+export default function useAxiosReq({ method = "GET", url, body, isLocalServer }) {
+    const [data, setData] = useState()
+    // טעינה בעת המתנה למידע
+    const [loading, setLoading] = useState(false)
+    // שגיאה
+    const [error, setError] = useState('')
+    // בקשת מידע
+    const fetchData = async () => {
+        // התחלת אפקט טעינה
+        setLoading(true)
+        try {
+            const result = await axiosReq({ method, url, body, isLocalServer })
+            setData(result)
+        } catch (e) {
+            setError(e)
+        } finally {
+            // הפסקת אפקט טעינה
+            setLoading(false)
+
+        }
+    }
+
+    // הפעלת קריאה
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return { data, loading, error, fetchData }
+}
