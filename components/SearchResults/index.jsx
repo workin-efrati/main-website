4fcca2ad6d1014d02ebb552c5622b0024add0edf
unknown <a0548428751@gmail.com>
2024-07-22 10:38:11 +0300

    ul li

--- components/SearchResults/index.jsx
+++ components/SearchResults/index.jsx
@@ -2,21 +2,32 @@ import styles from './styles.module.scss'
 import getData from '../jsonTest'
 import { useState, useEffect } from 'react';
 export default function SearchResults() {
-const [arr, setArr] = useState([])
+  const [arr, setArr] = useState([])
+
+  const getSubject = async () => {
+    const res = await getData();
+    // console.log(res);
+    setArr(res)
+  }
+
   useEffect(() => {
-    const getSubject = async () => {
-      const res = await getData();
-      setArr(res)
-    } 
-    const rav   = getSubject();
+    getSubject();
   }, [])
-  console.log(arr[0]?.subcategories[0]);
+  console.log(arr);
+
   return (
     <>
       <div className={styles.container}>
         {
-          // arr[0]?.map((item, index) => <h4>{item?.title}</h4>)
+
+          arr.map((item, index) => (
+            <ul key={index}>
+              <li className={styles.title}>{item.name}</li>
+            </ul>
+          ))
         }
+
+
 
       </div>
     </>
--- components/jsonTest.js
+++ components/jsonTest.js
@@ -1,129 +1,27 @@
 export default function getData() {
-  const jsonString = `[
+  const jsonString = [{
+    name: "שבת ומועדים", id: "@drfgr54g12g3",
+    childrens: [{
+      name: "הדלקת נרות", id: "@drfgr54g12g3",
+      childrens: [{}]
+  
+    }]
+
+  },
   {
-    "category": "Jewish Law",
-    "subcategories": [
-      {
-        "title": "Prayer",
-        "items": [
-          {
-            "title": "Shacharit Prayer",
-            "details": "Obligations and Times"
-          },
-          {
-            "title": "Mincha Prayer",
-            "details": "Obligations and Times"
-          },
-          {
-            "title": "Arvit Prayer",
-            "details": "Obligations and Times"
-          },
-          {
-            "title": "Blessings",
-            "items": [
-              {
-                "title": "Hamotzi Blessing",
-                "details": "Text and Times"
-              },
-              {
-                "title": "Birkat Hamazon",
-                "details": "Text and Times"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        "title": "Sabbath",
-        "items": [
-          {
-            "title": "Candle Lighting",
-            "details": "Times and Duties"
-          },
-          {
-            "title": "Sabbath Prohibitions",
-            "items": [
-              {
-                "title": "Labor",
-                "details": "Types of Prohibited Labor"
-              },
-              {
-                "title": "Ignition",
-                "details": "Prohibition of Ignition and Food Preparation"
-              },
-              {
-                "title": "Carrying in Public Domain",
-                "details": "Restrictions and Laws"
-              }
-            ]
-          },
-          {
-            "title": "Kiddush",
-            "details": "Text and Times"
-          }
-        ]
-      },
-      {
-        "title": "Kosher",
-        "items": [
-          {
-            "title": "Food Preparation",
-            "items": [
-              {
-                "title": "Challah Separation",
-                "details": "Duties and Laws"
-              },
-              {
-                "title": "Slaughter",
-                "details": "Requirements and Obligations"
-              }
-            ]
-          },
-          {
-            "title": "Mixing Milk and Meat",
-            "details": "Prohibitions and Laws"
-          }
-        ]
-      },
-      {
-        "title": "Festivals",
-        "items": [
-          {
-            "title": "Passover",
-            "items": [
-              {
-                "title": "Preparations",
-                "details": "Preparation for Passover and Differences"
-              },
-              {
-                "title": "Seder",
-                "details": "Stages and Customs"
-              }
-            ]
-          },
-          {
-            "title": "Sukkot",
-            "items": [
-              {
-                "title": "Building a Sukkah",
-                "details": "Requirements and Duties"
-              },
-              {
-                "title": "Lulav and Etrog",
-                "details": "Laws and Use"
-              }
-            ]
-          }
-        ]
-      }
-    ]
+    name: "בשר וחלב", id: "@drfgr54g12g3",
+    childrens: [{
+      name: "תערובת", id: "@drfgr54g12g3",
+      childrens: [{}]
+  
+    }]
+
   }
 ]
-
-`;
+    ;
 
   return new Promise((resolve) => {
-    const data = JSON.parse(jsonString);
+    const data = jsonString;
     resolve(data);
   });
 }
