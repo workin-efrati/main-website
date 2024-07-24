55b4390cdd1b931c54e2075206fae38abe73dd16
itamar <itamarpetel2002@gmail.com>
2024-07-24 12:28:11 +0300

    videosPage

--- app/allVideos/page.jsx
+++ app/allVideos/page.jsx
@@ -1,10 +1,22 @@
+
 import style from './style.module.scss'
-import CarouselVideos from '@/components/CarouselVideos'
-import data from './data.json'
-function page() {
-    return (<>
+import data1 from './data.json'
+import Link from 'next/link'
+import Image from 'next/image'
+import SearchVideos from '@/components/SearchVideos'
+function page({searchParams:{search}}) {
+const  data = data1.filter(d => d.title.includes(search))
+return (<>
+    
         <div className={style.page}>
-       <CarouselVideos videos={data}/>
+            <div>
+         <SearchVideos/>
+            </div>
+            <div className={style.holdVideos}>
+                {data?.map(v => <Link key={v.image}  className={style.card} href={v.link}><Image className={style.img} src={v.img}  fill alt="image" priority={1}/>
+                <div className={style.title}>{v.title}</div>
+                </Link>)}
+            </div>
         </div>
     </>)
 }
