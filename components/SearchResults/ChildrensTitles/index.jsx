1db58bb0a5ba98dcfc135142c28f9e83581b6640
unknown <a0548428751@gmail.com>
2024-07-24 16:34:16 +0300

    padding hover

--- components/SearchResults/ChildrensTitles/index.jsx
+++ components/SearchResults/ChildrensTitles/index.jsx
@@ -1,46 +1,40 @@
 import { FaMinus } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa";
-import { useState } from "react";
-import Text from '@/components/TextComponent'
 import styles from './styles.module.scss'
 import Link from "next/link";
-
+export const IconsPM = () => (<>
+    <FaMinus className={styles.minus} />
+    <FaPlus className={styles.plus} />
+  </>)
 export default function ChildrensTitles({ childrensData }) {
 
     return (
         <div className={styles.container}>
-            {
                 <ul>
                     {childrensData.map((child, index) => {
                         return <li key={child.id || index}>
                             <details >
                                 <summary>
-                                    <Text as="h5" textColor="blue">
                                         <div className={styles.title}>
-
-                                            {child.childrens.length > 0 ?
-                                                <>
-                                                    <FaMinus className={styles.minus} style={{ "width": "10px", "height": "10px" }} />
-                                                    <FaPlus className={styles.plus} style={{ width: "10px", height: "10px" }} />
-                                                </> :
-                                                <div style={{ width: "10px", height: "10px" }} />}
-                                            {child.childrens.length > 0 ? child.name : <a>{child.name}</a>}
+                                            {child.childrens.length > 0 ?<IconsPM />:
+                                                <div className={styles.placeHolderIcon} />}
+                                            {
+                                                <Link href={''}>
+                                                    {child.name}
+                                                </Link>
+                                            }
                                         </div>
-                                    </Text>
                                 </summary>
-                                {
                                     <div>
-                                        {
-                                            child?.childrens.length > 0 && <ChildrensTitles childrensData={child.childrens} />
+                                        {child?.childrens?.length > 0 &&
+                                         <ChildrensTitles childrensData={child.childrens} />
                                         }
-
                                     </div>
-                                }
                             </details>
                         </li>
                     })}
                 </ul>
-            }
+            
 
         </div>
     )
