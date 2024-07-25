1c782e4e2dec9a7b2901d68615017f48cd8a04c4
itamar <itamarpetel2002@gmail.com>
2024-07-25 12:25:33 +0300

    itamar videos

--- app/videos/page.jsx
+++ app/videos/page.jsx
@@ -1,21 +1,16 @@
 
-import style from './style.module.scss'
-import Link from 'next/link'
-import Image from 'next/image'
 import SearchVideos from '@/components/SearchVideos'
 import { connect } from '@/server/connect'
-import { readVidoes } from '@/server/services/vod.service'
+import { readVideos } from '@/server/services/vod.service'
+import Image from 'next/image'
+import Link from 'next/link'
+import style from './style.module.scss'
 
-export async function generateStaticParams() {
-    await connect()
-    const all = await readVidoes()
-    return all.map((vid) => ({ id: String(vid._id) }))
-}
 
 
 async function page({ searchParams: { search } }) {
     await connect()
-    const data = await readVidoes(search)
+    const data = await readVideos(search)
     return (<>
         <div className={style.page}>
             <div>
@@ -25,7 +20,7 @@ async function page({ searchParams: { search } }) {
                 {data?.map((v, i) =>
                     <Link key={v._id}
                         className={style.card}
-                        href={`/all-videos/${v._id}`}><Image
+                        href={`/videos/${v._id}`}><Image
                             className={style.img}
                             src={v.img}
                             fill
--- server/services/vod.service.js
+++ server/services/vod.service.js
@@ -1,10 +1,14 @@
-import { create, read } from "../controller/vod.controller"
+import { create, read,readOne } from "../controller/vod.controller"
 
 export const createVideo = async (data) => {
     const res = await create(data)
     return res
 }
-export const readVidoes = async (filter) => {
+export const readVideos = async (filter) => {
     const res = await read({ title: { $regex: new RegExp(filter || ''), $options: 'i'} })
+    return res
+}
+export const readOneVideo = async (filter) => {
+    const res = await readOne(filter)
     return res
 }
