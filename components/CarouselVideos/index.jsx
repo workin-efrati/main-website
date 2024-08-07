960784e4ba7a2cd82de08e4f142951e22c4df1f3
itamar <itamarpetel2002@gmail.com>
2024-08-07 20:17:58 +0300

    itamar - video (changes)

--- components/CarouselVideos/index.jsx
+++ components/CarouselVideos/index.jsx
@@ -1,25 +1,59 @@
-import Image from 'next/image'
-import style from './style.module.scss'
-import Link from 'next/link'
+import Image from "next/image";
+import style from "./style.module.scss";
+import Link from "next/link";
+import ButtonsForCarousel from "../ButtonsForCarousel";
 // [{text:"",image:"",link:""},{},{}]
-function CarouselVideos({ videos }) {
-    return (
-        <div className={style.holdVideos}>
-            {videos.map((i, n) =>
-                <div key={n} className={style.holdCard}>
-                    <Link href={i._id.toString()}>
-                        <Image
-                            alt="image"
-                            width={400}
-                            height={300}
-                            className={style.image}
-                            src={i.img}
-                        />
-                        <div className={style.text}>{i.title}</div>
-                    </Link>
-                </div>
-            )}
+function CarouselVideos({ videos=[] }) {
+// TODO - fix the carousel
+  return (
+    <>
+    <div className={style.carousel}>
+        <ButtonsForCarousel vidLenght={videos.length}>
+        {videos.map((i, n) => (
+          <div 
+            key={n}
+            className={style.holdCard}
+            >
+            <Link href={i._id.toString()}>
+              <Image
+                alt="image"
+                width={400}
+                height={300}
+                className={style.image}
+                src={i.img}
+                />
+              <div className={style.text}>{i.title}</div>
+            </Link>
+          </div>
+        ))}
+        </ButtonsForCarousel>
+    </div>
+
+
+
+    <div className={style.carouselPhone}>
+    <div className={style.holdVideos} >
+
+        {videos.map((i, n) => (
+          <div 
+          key={n}
+          className={style.holdCard}
+          >
+            <Link href={i._id.toString()}>
+              <Image
+                alt="image"
+                width={400}
+                height={300}
+                className={style.image}
+                src={i.img}
+                />
+              <div className={style.text}>{i.title}</div>
+            </Link>
+          </div>
+        ))}
         </div>
-    )
+    </div>
+        </>
+  );
 }
-export default CarouselVideos
+export default CarouselVideos;
